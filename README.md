3bf477a35b0d3bd3a33baf4c1e327dceb80f2026
timsainb <timsainb@gmail.com>
2019-10-06 17:35:56 -0700

    updates algorithms

--- README.md
+++ README.md
@@ -1,56 +1,55 @@
-vocalization_segmentation
+Vocalization segmentation
 ==============================
 
-Simple algorithms for segmenting vocalizations without supervision
+This is a set of simple algorithms for segmenting vocalizations without supervision using (many) parameters. It's basically meant as an easy dumb way of doing segmentation by eye so that you can repeat segmentation on many different vocalizations simultaneously using the same methods. 
 
-Project Organization
-------------
+There are a bunch of Jupyter notebook examples for European starlings, bengalese finch, canary, and mouse USVs. 
 
-    ├── LICENSE
-    ├── Makefile           <- Makefile with commands like `make data` or `make train`
-    ├── README.md          <- The top-level README for developers using this project.
-    ├── data
-    │   ├── external       <- Data from third party sources.
-    │   ├── interim        <- Intermediate data that has been transformed.
-    │   ├── processed      <- The final, canonical data sets for modeling.
-    │   └── raw            <- The original, immutable data dump.
-    │
-    ├── docs               <- A default Sphinx project; see sphinx-doc.org for details
-    │
-    ├── models             <- Trained and serialized models, model predictions, or model summaries
-    │
-    ├── notebooks          <- Jupyter notebooks. Naming convention is a number (for ordering),
-    │                         the creator's initials, and a short `-` delimited description, e.g.
-    │                         `1.0-jqp-initial-data-exploration`.
-    │
-    ├── references         <- Data dictionaries, manuals, and all other explanatory materials.
-    │
-    ├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
-    │   └── figures        <- Generated graphics and figures to be used in reporting
-    │
-    ├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
-    │                         generated with `pip freeze > requirements.txt`
-    │
-    ├── setup.py           <- makes project pip installable (pip install -e .) so src can be imported
-    ├── src                <- Source code for use in this project.
-    │   ├── __init__.py    <- Makes src a Python module
-    │   │
-    │   ├── data           <- Scripts to download or generate data
-    │   │   └── make_dataset.py
-    │   │
-    │   ├── features       <- Scripts to turn raw data into features for modeling
-    │   │   └── build_features.py
-    │   │
-    │   ├── models         <- Scripts to train models and then use trained models to make
-    │   │   │                 predictions
-    │   │   ├── predict_model.py
-    │   │   └── train_model.py
-    │   │
-    │   └── visualization  <- Scripts to create exploratory and results oriented visualizations
-    │       └── visualize.py
-    │
-    └── tox.ini            <- tox file with settings for running tox; see tox.testrun.org
+There are two main segmentation algorithms:
 
+1) <b>Dynamic thresholding:</b> segments syllables in time by computing a spectral envelope and modifying the threshold for segmentation on that envelope based on a set of parameters.
+
+<img src="assets/dynamic-thresholding.png" />
+
+2) <b>Continuous element segmentation:</b> segments elements of song spectro-temporally, so that two elements can be overlapping in time but not frequency. 
+
+<img src="assets/continuous-element.png" />
+
+
+### Parameters
+
+There are a whole bunch of different parameters for the two algorithms. Here is a quick description of each:
+
+```
+Arguments:
+        vocalization {[type]} -- waveform of song
+        rate {[type]} -- samplerate of datas
+
+    Keyword Arguments:
+        min_level_db {int} -- default dB minimum of spectrogram (threshold anything below) (default: {-80})
+        min_level_db_floor {int} -- highest number min_level_db is allowed to reach dynamically (default: {-40})
+        db_delta {int} -- delta in setting min_level_db (default: {5})
+        n_fft {int} -- FFT window size (default: {1024})
+        hop_length_ms {int} -- number audio of frames in ms between STFT columns (default: {1})
+        win_length_ms {int} -- size of fft window (ms) (default: {5})
+        ref_level_db {int} -- reference level dB of audio (default: {20})
+        pre {float} -- coefficient for preemphasis filter (default: {0.97})
+        spectral_range {[type]} -- spectral range to care about for spectrogram (default: {None})
+        verbose {bool} -- display output (default: {False})
+        mask_thresh_std {int} -- standard deviations above median to threshold out noise (higher = threshold more noise) (default: {1})
+        neighborhood_time_ms {int} -- size in time of neighborhood-continuity filter (default: {5})
+        neighborhood_freq_hz {int} -- size in Hz of neighborhood-continuity filter (default: {500})
+        neighborhood_thresh {float} -- threshold number of neighborhood time-frequency bins above 0 to consider a bin not noise (default: {0.5})
+        min_syllable_length_s {float} -- shortest expected length of syllable (default: {0.1})
+        min_silence_for_spec {float} -- shortest expected length of silence in a song (used to set dynamic threshold) (default: {0.1})
+        silence_threshold {float} -- threshold for spectrogram to consider noise as silence (default: {0.05})
+        max_vocal_for_spec {float} -- longest expected vocalization in seconds  (default: {1.0})
+        temporal_neighbor_merge_distance_ms {float} -- longest distance at which two elements should be considered one (default: {0.0})
+        overlapping_element_merge_thresh {float} -- proportion of temporal overlap to consider two elements one (default: {np.inf})
+        min_element_size_ms_hz {list} --  smallest expected element size (in ms and HZ). Everything smaller is removed. (default: {[0, 0]})
+        figsize {tuple} -- size of figure for displaying output (default: {(20, 5)})
+
+```
 
 --------
 
--- vocalseg/continuity_filtering.py
+++ vocalseg/continuity_filtering.py
@@ -1,3 +1,17 @@
+from tqdm.autonotebook import tqdm
+from vocalseg.utils import _normalize, spectrogram, norm, plot_spec
+from vocalseg.dynamic_thresholding import dynamic_threshold_segmentation
+import numpy as np
+from scipy import ndimage, signal
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+from matplotlib.colors import LinearSegmentedColormap
+
+cmap = matplotlib.colors.ListedColormap(np.random.rand(256, 3))
+cmap.set_bad(color=(0, 0, 0, 0))
+
+
 def continuity_segmentation(
     vocalization,
     rate,
@@ -15,7 +29,6 @@ def continuity_segmentation(
     neighborhood_time_ms=5,
     neighborhood_freq_hz=500,
     neighborhood_thresh=0.5,
-    max_element_len_ms=2000,
     min_syllable_length_s=0.1,
     min_silence_for_spec=0.1,
     silence_threshold=0.05,
@@ -27,44 +40,40 @@ def continuity_segmentation(
 ):
     """
     [summary]
-    
+
     Arguments:
-        vocalization {[type]} -- [description]
-        rate {[type]} -- [description]
-    
+        vocalization {[type]} -- waveform of song
+        rate {[type]} -- samplerate of datas
+
     Keyword Arguments:
-        min_level_db {int} -- [description] (default: {-80})
-        min_level_db_floor {int} -- [description] (default: {-40})
-        db_delta {int} -- [description] (default: {5})
-        n_fft {int} -- [description] (default: {1024})
-        hop_length_ms {int} -- [description] (default: {1})
-        win_length_ms {int} -- [description] (default: {5})
-        ref_level_db {int} -- [description] (default: {20})
-        pre {float} -- [description] (default: {0.97})
-        spectral_range {[type]} -- [description] (default: {None})
-        verbose {bool} -- [description] (default: {False})
-        mask_thresh_std {int} -- [description] (default: {1})
-        neighborhood_time_ms {int} -- [description] (default: {5})
-        neighborhood_freq_hz {int} -- [description] (default: {500})
-        neighborhood_thresh {float} -- [description] (default: {0.5})
-        max_element_len_ms {int} -- [description] (default: {2000})
-        min_syllable_length_s {float} -- [description] (default: {0.1})
-        min_silence_for_spec {float} -- [description] (default: {0.1})
-        silence_threshold {float} -- [description] (default: {0.05})
-        max_vocal_for_spec {float} -- [description] (default: {1.0})
-        temporal_neighbor_merge_distance_ms {float} -- [description] (default: {0.0})
-        overlapping_element_merge_thresh {[type]} -- [description] (default: {np.inf})
-        min_element_size_ms_hz {list} -- [description] (default: {[0, 0]})
-        hzfigsize {tuple} -- [description] (default: {(20, 5)})
-    
+        min_level_db {int} -- default dB minimum of spectrogram (threshold anything below) (default: {-80})
+        min_level_db_floor {int} -- highest number min_level_db is allowed to reach dynamically (default: {-40})
+        db_delta {int} -- delta in setting min_level_db (default: {5})
+        n_fft {int} -- FFT window size (default: {1024})
+        hop_length_ms {int} -- number audio of frames in ms between STFT columns (default: {1})
+        win_length_ms {int} -- size of fft window (ms) (default: {5})
+        ref_level_db {int} -- reference level dB of audio (default: {20})
+        pre {float} -- coefficient for preemphasis filter (default: {0.97})
+        spectral_range {[type]} -- spectral range to care about for spectrogram (default: {None})
+        verbose {bool} -- display output (default: {False})
+        mask_thresh_std {int} -- standard deviations above median to threshold out noise (higher = threshold more noise) (default: {1})
+        neighborhood_time_ms {int} -- size in time of neighborhood-continuity filter (default: {5})
+        neighborhood_freq_hz {int} -- size in Hz of neighborhood-continuity filter (default: {500})
+        neighborhood_thresh {float} -- threshold number of neighborhood time-frequency bins above 0 to consider a bin not noise (default: {0.5})
+        min_syllable_length_s {float} -- shortest expected length of syllable (default: {0.1})
+        min_silence_for_spec {float} -- shortest expected length of silence in a song (used to set dynamic threshold) (default: {0.1})
+        silence_threshold {float} -- threshold for spectrogram to consider noise as silence (default: {0.05})
+        max_vocal_for_spec {float} -- longest expected vocalization in seconds  (default: {1.0})
+        temporal_neighbor_merge_distance_ms {float} -- longest distance at which two elements should be considered one (default: {0.0})
+        overlapping_element_merge_thresh {float} -- proportion of temporal overlap to consider two elements one (default: {np.inf})
+        min_element_size_ms_hz {list} --  smallest expected element size (in ms and HZ). Everything smaller is removed. (default: {[0, 0]})
+        figsize {tuple} -- size of figure for displaying output (default: {(20, 5)})
+
     Returns:
-        [type] -- [description]
+        results -- a dictionary with results of segmentation
     """
 
     def plot_interim(spec, cmap=plt.cm.afmhot, zero_nan=False):
-        """
-        quickplot of a spectrogram
-        """
         fig, ax = plt.subplots(figsize=figsize)
         if zero_nan:
             spec = spec.copy()
@@ -91,12 +100,14 @@ def continuity_segmentation(
         min_level_db=min_level_db,
         db_delta=db_delta,
         silence_threshold=silence_threshold,
-        verbose=False,
+        verbose=verbose,
         spectral_range=spectral_range,
         min_syllable_length_s=min_syllable_length_s,
         min_silence_for_spec=min_silence_for_spec,
         max_vocal_for_spec=max_vocal_for_spec,
     )
+    if results is None:
+        return None
 
     spec = results["spec"]
 
@@ -185,10 +196,12 @@ def continuity_segmentation(
         if min_element_size > 0:
             elements = remove_small_elements(elements, min_element_size)
 
-            if verbose:
-                plot_interim(elements, cmap=cmap, zero_nan=True)
-                unique_elements = np.unique(elements[elements != 0].astype(int))
-                print("unique elements: {}".format(len(unique_elements)))
+    # randomize label values since they are temporally/frequency continuous
+    elements = randomize_labels(elements)
+    if verbose:
+        plot_interim(elements, cmap=cmap, zero_nan=True)
+        unique_elements = np.unique(elements[elements != 0].astype(int))
+        print("unique elements: {}".format(len(unique_elements)))
 
     results["elements"] = elements
 
@@ -196,15 +209,7 @@ def continuity_segmentation(
 
 
 def remove_small_elements(elements, min_element_size):
-    """
-    remove elements that are below some threshold size
-    
-    Arguments:
-        elements {[type]} -- [description]
-        min_element_size {[type]} -- [description]
-    
-    Returns:
-        [type] -- [description]
+    """ remove elements that are below some threshold size
     """
     # get unique points
     unique_elements = np.unique(elements[elements != 0].astype(int))
@@ -239,7 +244,6 @@ def merge_temporal_neighbors(
     Returns:
         [type] -- [description]
     """
-
     merged_elements = {}
     for element, st, et in tqdm(
         zip(unique_elements, syllable_start_times, syllable_end_times),
@@ -270,3 +274,204 @@ def merge_temporal_neighbors(
             syllable_end_times = np.delete(syllable_end_times, overlapping_syllables)
 
     return elements
+
+
+def merge_overlapping_elements(
+    elements,
+    unique_elements,
+    syllable_start_times,
+    syllable_end_times,
+    overlapping_element_merge_thresh,
+):
+    """
+    merge elements that are overlapping by at least overlapping_element_merge_thresh
+    
+    Arguments:
+        elements {[type]} -- [description]
+        unique_elements {[type]} -- [description]
+        syllable_start_times {[type]} -- [description]
+        syllable_end_times {[type]} -- [description]
+        overlapping_element_merge_thresh {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    # sort syllables by length
+    sort_mask = np.argsort(syllable_end_times - syllable_start_times)
+    syllable_end_times = syllable_end_times[sort_mask]
+    syllable_start_times = syllable_start_times[sort_mask]
+    unique_elements = unique_elements[sort_mask]
+
+    # loop through elements
+    for element, st, et in tqdm(
+        zip(unique_elements, syllable_start_times, syllable_end_times),
+        total=len(unique_elements),
+        desc="merging temporally overlapping elements",
+        leave=False,
+    ):
+        # elements have to be overlapped at least this length to merge
+        overlap_thresh = (et - st) * overlapping_element_merge_thresh
+
+        # get elements that
+        # # c1: start befre et - overlap_thresh and end after et,
+        #   c2: start before st and end after st + overlap_thresh
+        #   c3: or start after st and before et and are longer than overlap_thresh
+        #   c4: fully overlap syllable
+        c1 = (syllable_start_times < (et - overlap_thresh)) & (syllable_end_times > et)
+        c2 = (syllable_start_times < (st)) & (
+            syllable_end_times > (st + overlap_thresh)
+        )
+        c3 = ((syllable_start_times > (st)) & (syllable_end_times < et)) & (
+            (syllable_end_times - syllable_start_times) > overlap_thresh
+        )
+        c4 = (syllable_start_times < st) & (syllable_end_times > et)
+
+        # get list of overlapping elements
+        overlapping_syllables = np.where(c1 | c2 | c3 | c4)[0]
+
+        # print(overlapping_syllables)
+        if len(overlapping_syllables) > 0:
+            # get the longest syllable
+            overlapping_syllable = overlapping_syllables[-1]
+            syll_name = unique_elements[overlapping_syllable]
+
+            # change all elements to that element
+            elements[elements == element] = syll_name
+            # remove from lists
+            el = np.where(unique_elements == element)[-1]
+            unique_elements = np.delete(unique_elements, el)
+            syllable_start_times = np.delete(syllable_start_times, el)
+            syllable_end_times = np.delete(syllable_end_times, el)
+
+    return elements
+
+
+def randomize_labels(elements):
+    unique_elements = np.unique(elements[elements != 0].astype(int))
+    perm = np.random.permutation(unique_elements)
+    el_dict = {i: j for i, j in zip(unique_elements, perm)}
+    for el, val in el_dict.items():
+        elements[elements == el] = val
+    return elements
+
+
+def mask_spectrogram(spec, mask_thresh_std):
+    """
+    masks low power noise in a spectrogram
+    
+    Arguments:
+        spec {[type]} -- [description]
+        mask_thresh_std {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    return (
+        (
+            spec.T
+            < (np.median(spec, axis=1) + mask_thresh_std * np.std(spec, axis=1)) + 1e-5
+        )
+        .astype("float32")
+        .T
+    )
+
+
+def make_continuity_filter(
+    neighborhood_freq_hz, neighborhood_time_ms, spec_bin_hz, hop_length_ms
+):
+    """
+     Generate a filter for continuous elements
+    
+    Arguments:
+        neighborhood_freq_hz {[type]} -- [description]
+        neighborhood_time_ms {[type]} -- [description]
+        spec_bin_hz {[type]} -- [description]
+        hop_length_ms {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    n_bin_freq = int(neighborhood_freq_hz / spec_bin_hz)
+    n_bin_time = int(neighborhood_time_ms / hop_length_ms)
+    return np.ones((n_bin_freq, n_bin_time)) / np.product((n_bin_freq, n_bin_time))
+
+
+def segment_mask(mask):
+    """
+    segments a binary spectrogram mask into individual elements
+    
+    Arguments:
+        mask {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    elements, _ = ndimage.label(mask == False)
+    elements = np.ma.masked_where(elements == 0, elements)
+    elements = np.array(elements.data).astype("float32")
+    return elements
+
+
+def get_syllable_timing(elements, hop_length_ms):
+    """
+    gets length of elements of each mask type
+    
+    Arguments:
+        elements {[type]} -- [description]
+        hop_length_ms {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
+    # get unique points
+    unique_elements = np.unique(elements[elements != 0].astype(int))
+
+    # get the time coverage of each element
+    total_coverage = [
+        np.sum(elements == i, axis=0)
+        for i in tqdm(unique_elements, desc="element coverage", leave=False)
+    ]
+
+    # get the start and end times of each syllable
+    syllable_start_times, syllable_end_times = np.array(
+        [
+            np.where(i > 0)[0][np.array([0, -1])] + np.array([0.0, 1.0])
+            for i in tqdm(total_coverage, desc="element length", leave=False)
+        ]
+    ).T * float(hop_length_ms)
+
+    sort_mask = np.argsort(syllable_start_times)
+    syllable_start_times = syllable_start_times[sort_mask]
+    syllable_end_times = syllable_end_times[sort_mask]
+    unique_elements = unique_elements[sort_mask]
+
+    return unique_elements, syllable_start_times, syllable_end_times
+
+
+def plot_labelled_elements(elements, spec, figsize = (30,5)):
+    """ plots a spectrogram with colormap labels
+    """
+    unique_elements = np.unique(elements[elements != 0].astype(int))
+    pal = np.random.permutation(sns.color_palette('nipy_spectral', n_colors=len(unique_elements)))
+    
+    new_spec = np.ones(list(np.shape(elements)) + [4])
+    # fill spectrogram with colored regions
+    for el, pi in tqdm(zip(unique_elements, pal), total = len(unique_elements)):
+        cdict = {
+            'red': [(0, pi[0], pi[0]),(1, pi[0], pi[0])],
+            'green': [(0, pi[1], pi[1]),(1, pi[1], pi[1])],
+            'blue': [(0, pi[2], pi[2]),(1, pi[2], pi[2])],
+            'alpha': [(0, 0, 0),(1, 1, 1)]
+        }
+        cmap = LinearSegmentedColormap('CustomMap', cdict)
+        
+        new_spec[elements==el] = cmap(spec[elements==el])
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.imshow(new_spec,
+              interpolation=None,
+            aspect="auto",
+            origin="lower",)
+    
+    return new_spec
